5bdc22d7bdee80f1554bf5ac4229b6649435d910
Baptiste Florentin <baptiste@nefture.com>
2024-09-21 13:48:00 +0800

    Fork Safe's AllowanceModule
    https://github.com/safe-global/safe-modules/blob/a5aaf8efea96e352bc57a0a91f2e2b8b3862a0df/modules/allowances/contracts/AllowanceModule.sol

--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -2,23 +2,23 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import {MiniSafe} from "../src/MiniSafe.sol";
 
-contract CounterTest is Test {
-    Counter public counter;
+contract MiniSafeTest is Test {
+    MiniSafe public minisafe;
 
     function setUp() public {
-        counter = new Counter();
-        counter.setNumber(0);
+        minisafe = new MiniSafe();
+        minisafe.setNumber(0);
     }
 
     function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
+        minisafe.increment();
+        assertEq(minisafe.number(), 1);
     }
 
     function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+        minisafe.setNumber(x);
+        assertEq(minisafe.number(), x);
     }
 }
